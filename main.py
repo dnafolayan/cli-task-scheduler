46dc0bd6274b972ef8feff21274cbe5863895d58
dnafolayan <divineafolayan05@gmail.com>
2025-08-14 11:51:16 +0100

    add err_logging

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ curr_date = curr_datetime.date()
 
 res = subprocess.run(["ls", "-l"], capture_output=True, text=True)
 
-if res.returncode != 0:
-    print(res.stderr)
-
 command = ""
 for arg in res.args:
     command = command + arg + " "
 
+if res.returncode != 0:
+    with open("./logs/errors.log", "a") as err_log:
+        err_log.write(
+            f"Got err: {res.stderr} for {command.strip()} at {curr_time.hour}:{curr_time.minute}:{curr_time.second} on {curr_date}\n"
+        )
+    print(res.stderr)
+
+
 # update logs
 with open("./logs/command.log", "a") as log:
     log.write(
-        f"Ran {command.strip()} at {curr_time.hour}:{curr_time.minute}:{curr_time.second} on {curr_date}"
+        f"Ran {command.strip()} at {curr_time.hour}:{curr_time.minute}:{curr_time.second} on {curr_date}\n"
     )
